cc7bfec6d188284364d4ab9a521d3a653cb92ca0
chenyongzhiaaron <262667641@qq.com>
2018-04-20 17:36:38 +0800

    fixe interface case ok

--- run_tests.py
+++ run_tests.py
@@ -1,22 +1,15 @@
-import time
 import sys
+import time
+import unittest
 
 from HTMLTestRunner import HTMLTestRunner
-import unittest
 
 sys.path.append('./ProductManagement')
 sys.path.append('./db_fixture')
 
-
-
-from db_fixture import test_data
-
-
-
 # 指定测试用例为当前文件夹下的 interface 目录
 test_dir = './ProductManagement/'
 discover = unittest.defaultTestLoader.discover(test_dir, pattern='*_test.py')
-
 
 if __name__ == "__main__":
     # test_data.init_data() # 初始化接口测试数据
